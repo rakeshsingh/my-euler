f36f36b8bdec69f72755c7a7cccab446b2636fbc
Singh <raksingh@amazon.com>
2016-09-01 21:35:38 -0700

    added solution of 10

--- 001_multiples_of_3_and_5.py
+++ 001_multiples_of_3_and_5.py
@@ -7,6 +7,5 @@ def sum_of_multiples_of_3_5(n):
 			sum = sum + i 
 	return sum
 
-
 if __name__ == '__main__':
 	print(sum_of_multiples_of_3_5(1000))
--- 019_counting_sundays.py
+++ 019_counting_sundays.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python
 
-base_day=1
-base_day_of_week='Monday
-base_year=1900
-base_month=1
-day_num_map={
-    'Monday':1,
-    'Tuesday':2,
-    'Wednesday':3,
-    'Thursday':4,
-    'Friday':5,
-    'Saturday':6,
-    'Sunday':7 
-    }
-
+month_days={1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
 def is_leap(year):
     leap = False
     if year % 4 == 0:
@@ -22,12 +9,22 @@ def is_leap(year):
             leap = False
     return leap
 
-def first_day_of_month_count(day, start_year, end_year):
+def how_many_sundays(day, start_year, end_year):
     count = 0
-    for year in range(start_year, end_year):
-        if is_leap(year):
-
+    daynum=365
+    for year in range(start_year, end_year+1):
+        for month in range(1,13):
+            day_of_week=(daynum+1)%7
+            if day_of_week==0:
+                count+=1
+            print('First day of year {} and month {} is {}'.format(year, month, day_of_week))
+            #print(year,month)
+            if month ==2:
+                if is_leap(year):
+                    daynum+=1
+            daynum+=month_days[month]
+            #print(daynum)
     return count
 
 if __name__ == '__main__':
-    print(first_day_of_month_count('Sunday',1901,2000))
+    print(how_many_sundays('Sunday',1901,2000))
